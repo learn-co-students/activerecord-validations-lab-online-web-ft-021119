e6428112ecd07205f5c25f81437cd18b7fd2b807
Burak Kocak <burakkocak884@ghotmail.com>
2019-04-22 23:28:30 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: { minimum: 100}
+	validates :summary, length: { maximum: 50}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+	validate :clickbait?
+
+ def clickbait?
+ 	 if self.title && !self.title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+ 	 	errors.add(:clickbait, "")
+ 	 end
+ 	end
+
+
+
 end
