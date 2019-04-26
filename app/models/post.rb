5f8e47122c8402ec1c626073f56fd4b0ecb6a684
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-26 12:03:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, exclusion: { in: %w("Won't Believe", "Secret", "Top [number]", or "Guess"),
+    message: "%{value} is a required title bit." }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, exclusion: { in: %w("Fiction" "Non-Fiction"),
+    message: "%{value} is not a valid category option" }
 end
